a5d67ba9cd2d0654a5d005383d7205d96a52fbdc
Joseph-André Turk <josephandre.turk@gmail.com>
2023-10-31 13:17:47 +0100

    fixed web worker wasm package version for front end

---
+++ babygiant/pkg/index.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} x
+* @param {string} y
+* @param {bigint} min_range
+* @param {bigint} max_range
+* @returns {bigint}
+*/
+export function do_compute_dlog(x: string, y: string, min_range: bigint, max_range: bigint): bigint;
--- worker_babygiant.js
+++ worker_babygiant.js
@@ -1,13 +1,11 @@
 self.onmessage = async function(event) {
-    const wasm = await import("./babygiant/pkg/babygiant.js");
-    const init = wasm.default;
-    await init();
-    const { Cx, Cy, min_range, max_range } = event.data;
-    try {
-      const output = wasm.do_compute_dlog(Cx, Cy, BigInt(min_range), BigInt(max_range));
-      self.postMessage(output);
-    } catch (e) {
-      self.postMessage("dl_not_found");
-    }
-    self.close();
-  };
+  const wasm = await import("./babygiant/pkg/index.js");
+  const { Cx, Cy, min_range, max_range } = event.data;
+  try {
+    const output = wasm.do_compute_dlog(Cx, Cy, BigInt(min_range), BigInt(max_range));
+    self.postMessage(output);
+  } catch (e) {
+    self.postMessage("dl_not_found");
+  }
+  self.close();
+};
